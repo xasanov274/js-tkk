3293a90207ae0e826b11231cd56b453190bca5c1
xasanov274 <xasanovabdulaziz51@gmail.com>
2025-04-17 20:30:53 +0500

    tugadi

---
+++ main.js
@@ -0,0 +1,19 @@
+// console.log("toppa togri");
+
+// alert("Abdulaziz akov");
+
+// confirm("hovvv");
+
+// prompt("endi shuyoda yoziwamiza", "hoop");
+
+//console.log(23456);
+console.log(prompt('ismingizni kiriting'));
+console.log(prompt('familiyangizni kiriting'));
+console.log(prompt('yoshingizni kiriting'));
+console.log(prompt("qayerda yashashingizni kiriting"));
+console.log(prompt('yoqtirgan faningizni kiriting'));
+
+alert('rahmat')
+
+
+
