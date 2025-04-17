fd4313da2aecd088a3ef81fe1b5cd87f636a9cdc
xasanov274 <xasanovabdulaziz51@gmail.com>
2025-04-17 21:35:28 +0500

    tugadi

--- main.js
+++ main.js
@@ -7,13 +7,30 @@
 // prompt("endi shuyoda yoziwamiza", "hoop");
 
 //console.log(23456);
-console.log(prompt('ismingizni kiriting'));
-console.log(prompt('familiyangizni kiriting'));
-console.log(prompt('yoshingizni kiriting'));
-console.log(prompt("qayerda yashashingizni kiriting"));
-console.log(prompt('yoqtirgan faningizni kiriting'));
 
-alert('rahmat')
+let ism = prompt("ismingizni kiriting:")
+let familiya = prompt("familiyangizni kiriting:")
+let yosh = prompt("yoshingizni kiriting:")
+let manzil = prompt("qayerda yashashingizni kiriting:")
+let fan = prompt("yoqtirgan faningizni kiriting:","IT")
+
+console.log("ism:" + ism);
+console.log("familiya:" + familiya);
+console.log("yosh:" + yosh);
+console.log("manzil:" + manzil);
+console.log("fan:" + fan);
+
+
+
+
+
+// console.log(prompt('ismingizni kiriting'));
+// console.log(prompt('familiyangizni kiriting'));
+// console.log(prompt('yoshingizni kiriting'));
+// console.log(prompt('qayerda yashashingizni kiriting'));
+// console.log(prompt("yoqtirgan faningizni kiriting","IT"));
+
+// alert('rahmat')
 
 
 
